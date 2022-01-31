896bbbdb3396e5f995c587194c29abac8ca430b5
Bevzyk Soi <bevzyk.soi@gmail.com>
2022-01-31 21:00:28 +0200

    Added form

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import Phonebook from './components/Phonebook/Phonebook';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <Phonebook />
+    );
+  }
+};
